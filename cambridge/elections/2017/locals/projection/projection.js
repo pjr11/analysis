38be194b11d3a2d4700bc78202e9fdba95113547
Phil Rodgers <phil@philrodgers.co.uk>
2017-04-29 10:23:16 +0100

    Add historic results and parsing code

---
+++ cambridge/elections/2017/locals/projection/projection.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+// Data is Ward,Party,2016,2015,2014,2013,2012
+
+const readData = () => {
+  const file = fs.readFileSync('./results.csv', { encoding: 'utf8' });
+  const lines = file.split('\n');
+  const data = {};
+
+  lines.forEach(line => {
+    const cols = line.split(',');
+    const ward = cols[0];
+    const party = cols[1];
+    for (var i = 2; i <=6; i++) {
+      var votes = cols[i];
+      if (votes) {
+        const year = 2018 - i;
+        data[year] = data[year] || {};
+        const yearData = data[year];
+        yearData[ward] = yearData[ward] || {};
+        const wardData = yearData[ward];
+        wardData[party] = votes;
+      }
+    }
+  });
+
+  return data;
+};
+
+console.log(JSON.stringify(readData(), null, 2));

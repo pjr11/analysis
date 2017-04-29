2e2816a25d1614e4a30373a89da3bf57d7673d96
Phil Rodgers <phil@philrodgers.co.uk>
2017-04-29 11:57:32 +0100

    Add projection code

--- cambridge/elections/2017/locals/projection/projection.js
+++ cambridge/elections/2017/locals/projection/projection.js
@@ -64,9 +64,10 @@ var mapping = {
 // This code reads in the actual local election results for
 // 2012-2016, and produces projected results for the new
 // boundaries.
-
+//
 // Data is Ward,Party,2016,2015,2014,2013,2012
-
+//
+// We read it into the format { year: { ward: { party: votes }}}
 
 const readData = () => {
   const file = fs.readFileSync('./results.csv', { encoding: 'utf8' });
@@ -74,7 +75,7 @@ const readData = () => {
   const data = {};
 
   lines.forEach(line => {
-    const cols = line.split(',');
+    cols = line.split(',');
     const ward = cols[0];
     const party = cols[1];
     for (var i = 2; i <=6; i++) {
@@ -93,12 +94,84 @@ const readData = () => {
   return data;
 };
 
+// Make projected results for each division in each year in the same format,
+// though with percentages instead of total votes
 const makeProjections = () => {
+  const projections = {};
+  const data = readData();
+  const years = Object.keys(data);
+  const divisions = Object.keys(mapping);
 
+  years.forEach(year => {
+    const projection = {};
+    projections[year] = projection;
+
+    divisions.forEach(division => {
+      const votes = {};
+      const wards = mapping[division];
+
+      Object.keys(wards).forEach(ward => {
+        const percent = wards[ward];
+        const wardResult = data[year][ward];
+        const parties = Object.keys(wardResult);
+        parties.forEach(party => {
+          votes[party] = (votes[party] || 0) + wardResult[party] * percent;
+        });
+      });
+
+      const allParties = Object.keys(votes);
+      let totalVotes = 0;
+      allParties.forEach(party => {
+        totalVotes += votes[party];
+      });
+      allParties.forEach(party => {
+        votes[party] /= totalVotes;
+      });
+      projection[division] = votes;
+    });
+  });
+
+  return projections;
 };
 
+const allPartiesStanding = (projections, division) => {
+  var parties = {};
+  var years = Object.keys(projections);
+  years.forEach(year => {
+    const votes = projections[year][division];
+    const yearParties = Object.keys(votes);
+    yearParties.forEach(party => {
+      parties[party] = true;
+    });
+  });
+
+  return Object.keys(parties);
+};
+
+// Convert the results to csv format - this time with years in
+// increasing order. So
+// ward,party,2012,2013,2014,2015,2016
 const makeCSV = () => {
+  const fromYear = 2012;
+  const toYear = 2016;
+  const divisions = Object.keys(mapping);
+  const projections = makeProjections();
+  const lines = [];
 
+  divisions.forEach(division => {
+    const parties = allPartiesStanding(projections, division);
+    parties.forEach(party => {
+      const line = [division, party];
+      for (let year = fromYear; year <= toYear; year++) {
+        let tyear = '' + year;
+        const votes = '' + (projections[tyear][division][party] || '');
+        line.push(votes);
+      }
+      lines.push(line.join(','));
+    });
+  });
+
+  return lines.join('\n');
 };
 
-console.log(JSON.stringify(readData(), null, 2));
+console.log(makeCSV());
